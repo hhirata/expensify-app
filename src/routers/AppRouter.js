c5b43fc49d3e31e0424509c7148c4da01cd33cd2
Henrique Hirata <henrique@sydy.com.br>
2018-01-04 17:31:53 -0300

    add public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,20 +4,19 @@ import createHistory from 'history/createBrowserHistory'
 import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
-import HelpPage from '../components/HelpPage'
 import LoginPage from '../components/LoginPage'
 import NotFoundPage from '../components/NotFoundPage'
 import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 export const history = createHistory()
 const AppRouter = () =>  (
     <Router history={history}>
     <div>
         <Switch>
-            <Route path="/" component={LoginPage} exact={true}/>
+            <PublicRoute path="/" component={LoginPage} exact={true}/>
             <PrivateRoute path="/create" component={AddExpensePage} />
             <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
             <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
-            <Route path="/help" component={HelpPage}/>
             <Route component={NotFoundPage} />
         </Switch>
     </div>
